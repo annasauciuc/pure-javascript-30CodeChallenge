abe4eb702e3c76c8369236f37fc2bd6be5700b89
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-25 11:54:24 +0000

    adding the drum functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-window.addEventListener("keydown",function(event){
-console.log('event :', event);
-})
+const removeTransition = e => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+};
+const playSound = e => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+};
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
